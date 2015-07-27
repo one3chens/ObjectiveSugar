6cd930142fda3357076178b72f7123af6554ce15
Brentley Jones <Brentley.Jones@target.com>
2015-07-27 14:11:17 -0500

    Add dynamic frameworks

--- Classes/ObjectiveSugar.h
+++ Classes/ObjectiveSugar.h
@@ -1,6 +1,16 @@
+
+#import <Foundation/Foundation.h>
+
 // C SUGAR
 #define unless(condition...) if(!(condition))
 #define until(condition...) while(!(condition))
+
+//! Project version number for ObjectiveSugar.
+FOUNDATION_EXPORT double ObjectiveSugarVersionNumber;
+
+//! Project version string for ObjectiveSugar.
+FOUNDATION_EXPORT const unsigned char ObjectiveSugarVersionString[];
+
 
 // OBJC SUGAR
 #import "NSNumber+ObjectiveSugar.h"
