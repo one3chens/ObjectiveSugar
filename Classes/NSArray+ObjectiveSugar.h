76ecc690fe991d5a5eb8fcb49d4b62dcf3620c7f
Brentley Jones <Brentley.Jones@target.com>
2015-07-27 14:17:41 -0500

    Fix missing import

--- Classes/NSArray+ObjectiveSugar.h
+++ Classes/NSArray+ObjectiveSugar.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+
 // For an overview see http://cocoadocs.org/docsets/ObjectiveSugar/
 
 @interface NSArray (ObjectiveSugar)
